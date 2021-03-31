50d89c11a5f40efdb13950c4edb31fc60e32c6db
Dbirtic <Dominik.birtic666@gmail.com>
2021-03-31 20:17:29 +0200

    get and post request form with ajax

--- process.php
+++ process.php
@@ -1,3 +1,11 @@
 <?php
 
 echo 'Processing...';
+
+// Check for GET variable
+if(isset($_GET['name'])){
+    echo 'GET: Your name is '. $_GET['name'];
+}
+if(isset($_POST['name'])){
+    echo 'Post: Your name is: '. $_POST['name'];
+}
